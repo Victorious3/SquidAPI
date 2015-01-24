b9d5219d15dfa31ed31da39b0bb990ace3169a12
coolsquid <snakkeflink@gmail.com>
2015-01-24 16:11:38 +0100

    Refactoring

--- java/com/github/coolsquid/squidlib/creativetab/SquidCreativeTabs.java
+++
@@ -1,9 +0,0 @@
-package com.github.coolsquid.squidlib.creativetab;
-
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
-
-
-public class SquidCreativeTabs {
-	public static final ITab squidTab = new ITab("SquidTab", Item.getItemFromBlock(Blocks.water));
-}
--- java/com/github/coolsquid/squidlib/exception/RegistryException.java
+++
@@ -1,9 +0,0 @@
-package com.github.coolsquid.squidlib.exception;
-
-public class RegistryException extends SquidAPIException {
-	
-	private static final long serialVersionUID = 1879823L;
-	public RegistryException(String comment) {
-		super(comment);
-	}
-}

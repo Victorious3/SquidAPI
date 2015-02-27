9645916e74d5714f41eed118c6bff6f79bd1dffe
coolsquid <snakkeflink@gmail.com>
2015-02-27 17:37:06 +0100

    Copyright headers

--- java/com/github/coolsquid/squidapi/chat/ChatUtils.java
+++ java/com/github/coolsquid/squidapi/chat/ChatUtils.java
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2015 CoolSquid.
+ * All rights reserved.
+ *******************************************************************************/
 package com.github.coolsquid.squidapi.chat;
 
 import com.github.coolsquid.squidapi.helpers.server.ServerHelper;
